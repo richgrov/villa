e2a48bd3b08abe716007d5dc64e1c6f0aa86b99d
unknown <38992201+richgrov@users.noreply.github.com>
2024-04-15 22:56:51 -0600

    Add SIMULO_DEBUG_ASSERT

---
+++ src/util/debug_assert.h
@@ -0,0 +1,26 @@
+#ifndef SIMULO_UTIL_DEBUG_ASSERT_H_
+#define SIMULO_UTIL_DEBUG_ASSERT_H_
+
+#ifdef NDEBUG
+
+#define SIMULO_DEBUG_ASSERT(cond, fmt, ...)
+
+#else // NDEBUG
+
+#include <cstdlib>
+#include <format>
+#include <iostream>
+#include <utility>
+
+#define SIMULO_DEBUG_ASSERT(cond, fmt, ...)                                                        \
+   {                                                                                               \
+      if (!(cond)) {                                                                               \
+         std::cerr << __FILE__ << ":" << __LINE__ << ": " << std::format(fmt, __VA_ARGS__)         \
+                   << "\n";                                                                        \
+         std::exit(1);                                                                             \
+      }                                                                                            \
+   }
+
+#endif // !NDEBUG
+
+#endif // !SIMULO_UTIL_DEBUG_ASSERT_H_
