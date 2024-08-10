9c6d4fcc9d9c0de7849fcf27e1b0cb09ecba50b9
unknown <38992201+richgrov@users.noreply.github.com>
2024-08-10 00:53:58 -0600

    Update slab test to use new api

--- src/util/slab_test.cc
+++ src/util/slab_test.cc
@@ -3,109 +3,69 @@
 #include "slab.h"
 
 TEST_CASE("Slab insert/get/release") {
-   int progress = 0;
-
-   class Test {
-   public:
-      int a_;
-      short b_;
-      int *progress_;
-
-      Test(int a, short b, int *progress) : a_(a), b_(b), progress_(progress) {
-         (*progress_)++;
-      }
-
-      ~Test() {
-         (*progress_)--;
-      }
+   struct Test {
+      int a;
+      short b;
    };
 
-   simulo::Slab<Test, 256> slab;
+   Slab<Test, 256> slab;
 
-   int key1 = slab.emplace(1, 2, &progress);
-   CHECK(progress == 1);
-   int key2 = slab.emplace(3, 4, &progress);
-   CHECK(progress == 2);
+   int key1 = slab.alloc_zeroed();
+   CHECK(key1 != SIMULO_INVALID_SLAB_KEY);
+   int key2 = slab.alloc_zeroed();
+   CHECK(key2 != SIMULO_INVALID_SLAB_KEY);
 
    {
       auto &test1 = slab.get(key1);
-      CHECK(progress == 2);
-      CHECK(test1.a_ == 1);
-      CHECK(test1.b_ == 2);
-      CHECK(test1.progress_ == &progress);
+      CHECK(test1.a == 0);
+      CHECK(test1.b == 0);
 
-      test1.a_ = 4;
-      test1.b_ = -1;
-      test1.progress_ = &progress;
+      test1.a = 4;
+      test1.b = -1;
 
       auto &test2 = slab.get(key2);
-      CHECK(progress == 2);
-      CHECK(test2.a_ == 3);
-      CHECK(test2.b_ == 4);
-      CHECK(test2.progress_ == &progress);
+      CHECK(test2.a == 0);
+      CHECK(test2.b == 0);
    }
-
-   CHECK(progress == 2);
 
    {
       auto &test = slab.get(key1);
-      CHECK(test.a_ == 4);
-      CHECK(test.b_ == -1);
+      CHECK(test.a == 4);
+      CHECK(test.b == -1);
    }
 
    slab.release(key1);
-   CHECK(progress == 1);
    slab.release(key2);
-   CHECK(progress == 0);
 }
 
-TEST_CASE("Slab fill, ctor/dtor") {
-   int used = 0;
-
-   class Test {
-   public:
-      int *used_;
-
-      Test(int *used) : used_(used) {
-         (*used_)++;
-      }
-
-      ~Test() {
-         (*used_)--;
-      }
+TEST_CASE("Slab fill/empty") {
+   struct Test {
+      int unused;
    };
 
    {
-      simulo::Slab<Test, 256> slab;
+      Slab<Test, 256> slab;
 
       for (int i = 0; i < 256; ++i) {
-         int key = slab.emplace(&used);
-         CHECK(key != simulo::kInvalidSlabKey);
+         CHECK(slab.alloc_zeroed() != SIMULO_INVALID_SLAB_KEY);
       }
-      CHECK(used == 256);
 
       for (int i = 0; i < 256; ++i) {
          if (i % 2 == 0) {
             slab.release(i);
          }
       }
-      CHECK(used == 128);
 
       for (int i = 0; i < 256; ++i) {
          if (i % 2 == 0) {
-            int key = slab.emplace(&used);
-            CHECK(key != simulo::kInvalidSlabKey);
+            CHECK(slab.alloc_zeroed() != SIMULO_INVALID_SLAB_KEY);
          }
       }
-      CHECK(used == 256);
 
       for (int i = 255; i >= 0; --i) {
          if (i % 2 == 1) {
             slab.release(i);
          }
       }
-      CHECK(used == 128);
    }
-
-   CHECK(used == 0);
 }
