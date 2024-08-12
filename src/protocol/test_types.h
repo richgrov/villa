b9f4b330d8ad1f3ad51d79297b958ca32c524e02
unknown <38992201+richgrov@users.noreply.github.com>
2024-08-12 00:26:35 -0600

    Add test for read_mc_long

--- src/protocol/test_types.h
+++ src/protocol/test_types.h
@@ -1,4 +1,5 @@
 #include "protocol/types.h"
+#include "util/os_detect.h"
 #include "util/test_assert.h"
 
 void test_read_protocol_types() {
@@ -7,4 +8,12 @@ void test_read_protocol_types() {
 
    unsigned char int_buf[] = {0x44, 0xE1, 0x11, 0xA7};
    assert_eq_i32(1155600807, read_mc_int(int_buf));
+
+   unsigned char long_buf[] = {0xFF, 0x10, 0x7C, 0x99, 0x00, 0x65, 0x9A, 0x0D};
+#ifdef SIMULO_WINDOWS
+   int64_t expected_long = -67416997832058355LL;
+#else
+   int64_t expected_long = -67416997832058355L;
+#endif
+   assert_eq_i64(expected_long, read_mc_long(long_buf));
 }
--- src/util/test_assert.h
+++ src/util/test_assert.h
@@ -4,6 +4,7 @@
 #include <stdint.h>
 
 #include "util/debug_assert.h"
+#include "util/os_detect.h"
 
 static void assert_eq_i16(int16_t expected, int16_t actual) {
    ASSERT(expected == actual, "%d != %d", expected, actual);
@@ -11,6 +12,14 @@ static void assert_eq_i16(int16_t expected, int16_t actual) {
 
 static void assert_eq_i32(int32_t expected, int32_t actual) {
    ASSERT(expected == actual, "%d != %d", expected, actual);
+}
+
+static void assert_eq_i64(int64_t expected, int64_t actual) {
+#ifdef SIMULO_WINDOWS
+   ASSERT(expected == actual, "%lld != %lld", expected, actual);
+#else
+   ASSERT(expected == actual, "%ld != %ld", expected, actual);
+#endif
 }
 
 #endif // !SIMULO_UTIL_TEST_ASSERT_H_
