d96fd13fdaf582c61b3c3f65c4aac24528e491f4
unknown <38992201+richgrov@users.noreply.github.com>
2024-09-09 10:25:03 -0600

    Add assert_eq_bytes

--- src/util/test_assert.h
+++ src/util/test_assert.h
@@ -22,4 +22,25 @@ static void assert_eq_i64(int64_t expected, int64_t actual) {
 #endif
 }
 
+static void assert_eq_bytes(
+   unsigned char *expected, size_t expected_len, unsigned char *actual, size_t actual_len
+) {
+#ifdef SIMULO_WINDOWS
+   ASSERT(expected_len == actual_len, "expected %lld bytes but got %lld", expected_len, actual_len);
+#else
+   ASSERT(expected_len == actual_len, "expected %ld bytes but got %ld", expected_len, actual_len);
+#endif
+
+   for (size_t i = 0; i < expected_len; ++i) {
+      unsigned char exp = expected[i];
+      unsigned char act = actual[i];
+
+#ifdef SIMULO_WINDOWS
+      ASSERT(exp == act, "expected byte %lld to be %d but got %d", i, exp, act);
+#else
+      ASSERT(ex == ac, "expected byte %ld to be %d but got %d", i, ex, ac);
+#endif
+   }
+}
+
 #endif // !SIMULO_UTIL_TEST_ASSERT_H_
