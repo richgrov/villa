36b24fe713bcd56c1dad90e20e2e168d742af2b1
unknown <38992201+richgrov@users.noreply.github.com>
2024-09-09 10:25:16 -0600

    Add tests for int/long serialization

--- src/protocol/test_types.h
+++ src/protocol/test_types.h
@@ -17,3 +17,19 @@ void test_read_protocol_types() {
 #endif
    assert_eq_i64(expected_long, read_mc_long(long_buf));
 }
+
+void test_write_protocol_types() {
+   unsigned char write_buf[8];
+
+   write_mc_int(write_buf, 1155600807);
+   unsigned char int_expected[] = {0x44, 0xE1, 0x11, 0xA7};
+   assert_eq_bytes(int_expected, sizeof(int_expected), write_buf, 4);
+
+#ifdef SIMULO_WINDOWS
+   write_mc_long(write_buf, -67416997832058355LL);
+#else
+   write_mc_long(write_buf, -67416997832058355L);
+#endif
+   unsigned char long_expected[] = {0xFF, 0x10, 0x7C, 0x99, 0x00, 0x65, 0x9A, 0x0D};
+   assert_eq_bytes(long_expected, sizeof(long_expected), write_buf, 8);
+}
--- src/test_main.c
+++ src/test_main.c
@@ -1,6 +1,10 @@
+#include <stdio.h>
+
 #include "protocol/test_types.h"
 
 int main(int argc, char **argv) {
    test_read_protocol_types();
+   test_write_protocol_types();
+   puts("OK");
    return 0;
 }
